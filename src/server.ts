69454ceacd0f941504537644ee4b982c250f0930
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-09-27 11:45:31 -0300

    Criando o método Post do User

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,30 @@ import { updateData, getdataBaseArray } from './data/database';
 const app = express();
 app.use(express.json());
 
+//Metódo get: (User)
 app.get("/users", (req, res)=>{
-    res.status(201).json(getdataBaseArray());
+    res.status(200).json(getdataBaseArray());
+});
+
+//Método post: (User)
+app.post("/users", (req, res)=>{
+    const {name, username} = req.body;
+    const userNameExist = getdataBaseArray().some((item) => item.userName === username);
+    //Confirmando se o userName já existe ou não:
+    if(userNameExist){
+        res.status(400).json({"error": "Existing UserName"});
+    }else{
+        //Criando User:
+        const newUser:User = {
+            id: uuidv4(),
+            name : name,
+            userName: username,
+            technologies: [],
+        }
+        //Atualizando o dataBase:
+        updateData(newUser);
+        res.status(201).json(newUser);
+    }
 });
 
 app.listen(5000, ()=>{
