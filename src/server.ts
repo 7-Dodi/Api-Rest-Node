b8fdc55005630b9111a8ec179dee5f7cabf52a13
Douglas Silva <douglassilvaaraujo78@gmail.com>
2023-09-27 10:40:15 -0300

    Construindo os tipos de dados

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,22 @@ import {v4 as uuidv4} from 'uuid';
 const app = express();
 app.use(express.json());
 
+//Construindo os tipos:
+type Technologies = {
+    id: string;
+    title: string;
+    studied: boolean;
+    deadline: Date;
+    created_at: Date;
+}
+
+type User = {
+    id: string;
+    name: string;
+    userName: string;
+    technologies: Technologies[];
+}
+
 app.listen(5000, ()=>{
     console.log("Servidor funcionando na porta 5000");
 })
